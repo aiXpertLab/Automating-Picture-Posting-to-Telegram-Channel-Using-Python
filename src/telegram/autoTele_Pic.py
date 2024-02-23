1e2fa8fb06a07447b25665c51ab0ba0a87497c0f
aiXpertLab <aiXpertLab@gmail.com>
2024-02-22 22:24:42 -0500

    final

--- src/telegram/autoTele_Pic.py
+++ src/telegram/autoTele_Pic.py
@@ -1,14 +1,16 @@
 import os, random, shutil, telebot
+from dotenv import load_dotenv
 
 from utils.utilities import RandomTitleGenerator
 
+load_dotenv()
 BOT_TOKEN = os.environ.get("TELE_TOKEN")
 bot = telebot.TeleBot(BOT_TOKEN)
 
-channel_name = "-1002126729483"   #asiagirl
-# channel_name = "-4061783014"        #ycc
+channel_name1 = "-1002126729483"   #asiagirl
+# channel_name2 = "-4061783014"        #ycc
 
-caption = RandomTitleGenerator("./src/telegram/resources/captions.txt")
+caption = RandomTitleGenerator("a:/autoTele/src/telegram/resources/captions.txt")
 
 source="E:/gDrive/38.Pic/Tele"
 dest  ="E:/gDrive/38.Pic/Tele/web"
@@ -19,6 +21,6 @@ shutil.move(source_file,dest)
 dest_file="%s/%s"%(dest,random_file)
 photo = open(dest_file, 'rb')
 
-bot.send_photo(channel_name, photo, caption=caption.get_random_line())
+bot.send_photo(channel_name1, photo, caption=caption.get_random_line())
 # bot.send_message(chat_id=channel_name, text="day day asian girl 3")
 # bot.infinity_polling()     
